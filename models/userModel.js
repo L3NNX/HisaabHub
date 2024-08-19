5e05432367055166f0b1c69d6f60b35c719a8084
L3NNX <gauravboy1122@gmail.com>
2024-08-20 01:58:42 +0530

    MongoDb

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose")
 
-mongoose.connect('mongodb://127.0.0.1:27017/khaatabook')
+mongoose.connect('mongodb+srv://Khaatabook:1234@khaatabook.dokzs.mongodb.net/?retryWrites=true&w=majority&appName=Khaatabook')
 
 const userSchema = new mongoose.Schema({
     username: {
