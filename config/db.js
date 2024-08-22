1918b240369e5ca222448e000bac4cd2257bc749
L3NNX <gauravboy1122@gmail.com>
2024-08-23 00:59:49 +0530

    Uploading

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-      
-        await mongoose.connect(process.env.MONGODB_URI); // Removed deprecated options
+        await mongoose.connect(process.env.MONGODB_URI); 
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('MongoDB connection error:', err);
