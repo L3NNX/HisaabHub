23e5872b560b722897e3eca155ee30e2655511b6
L3NNX <gauravboy1122@gmail.com>
2024-08-22 22:58:21 +0530

    Uploading

--- middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-function isAuthenticated(req, res, next) {
-    if (req.session.userId) {
-        // User is authenticated, proceed to the next middleware/route
-        return next();
-    }
-    res.redirect('/login'); 
-}
-
-module.exports = isAuthenticated; 
