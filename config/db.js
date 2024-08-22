67614f1f34444913598df7b4ee1fbdb118b0b6f6
L3NNX <gauravboy1122@gmail.com>
2024-08-23 00:44:07 +0530

    Uploading

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        
+        console.log('MongoDB URI:', "mongodb+srv://Khaatabook:1234@khaatabook.dokzs.mongodb.net/?retryWrites=true&w=majority&appName=Khaatabook");
         await mongoose.connect(process.env.MONGODB_URI); // Removed deprecated options
         console.log('MongoDB connected successfully');
     } catch (err) {
